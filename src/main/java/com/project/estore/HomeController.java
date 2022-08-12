c3cbbeadbe2e768a648fb457dabf741992c96ddd
blue-ayye <ccoolbuddy4@gmail.com>
2022-08-13 00:14:47 +0530

    using ModelAndView with and without Map

--- src/main/java/com/project/estore/HomeController.java
+++ src/main/java/com/project/estore/HomeController.java
@@ -4,14 +4,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String homePage(ModelMap model) {
-        model.addAttribute("firstName", "Sharath")
-                .addAttribute("lastName", "M A");
-        return "home";
+    public ModelAndView homePage() {
+
+//        With Map
+//        Map<String, String> model = new HashMap<>();
+//        model.put("firstName", "Sharath");
+//        model.put("lastName", "M A");
+//
+//        ModelAndView modelAndView = new ModelAndView("home", model);
+
+//        Without Map
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("home");
+        modelAndView.addObject("firstName", "sharath");
+        modelAndView.addObject("lastName", "m a");
+        return modelAndView;
     }
 }
