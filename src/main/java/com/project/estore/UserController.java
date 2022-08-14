5fe66b90bb6c6ec4ecf7c91b47fe4c6b23520b4c
blue-ayye <ccoolbuddy4@gmail.com>
2022-08-14 20:08:09 +0530

    Read query string parameter with @RequestParam

--- src/main/java/com/project/estore/UserController.java
+++ src/main/java/com/project/estore/UserController.java
@@ -3,6 +3,7 @@ package com.project.estore;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -14,6 +15,13 @@ public class UserController {
         modelAndView
                 .addObject("userId", userId)
                 .addObject("albumId", albumId);
+        return modelAndView;
+    }
+
+    @GetMapping(path = "/users")
+    public ModelAndView getUsers(@RequestParam(name = "limit", defaultValue = "30") int limit) {
+        System.out.println("Limit is : " + limit);
+        ModelAndView modelAndView = new ModelAndView("users");
         return modelAndView;
     }
 }
